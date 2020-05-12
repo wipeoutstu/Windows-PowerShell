ed613577d49c5708e4365d4c178b82ea3fe57572
Stuart Woods <stuartwoods@outlook.com>
2020-05-12 18:05:04 +0100

    Create Move Files to folders based on month & year.ps1

---
+++ Move Files to folders based on month & year.ps1
@@ -0,0 +1,66 @@
+<<<<<<< HEAD
+﻿# Get the files which should be moved, without folders
+$files = Get-ChildItem "H:\## Camera Uploads ##\" -Recurse | where {!$_.PsIsContainer}
+ 
+# List Files which will be moved
+$files
+ 
+# Target Folder where files should be moved to. The script will automatically create a folder for the year and month.
+$targetPath = "H:\## Camera Uploads ##\"
+ 
+foreach ($file in $files)
+{
+# Get year and Month of the file
+# I used LastWriteTime since this are synced files and the creation day will be the date when it was synced
+$year = $file.LastWriteTime.Year.ToString()
+$month = $file.LastWriteTime.Month.ToString()
+ 
+# Out FileName, year and month
+$file.Name
+$year
+$month
+ 
+# Set Directory Path
+$Directory = $targetPath + "\" + $year + "\" + $month
+# Create directory if it doesn't exsist
+if (!(Test-Path $Directory))
+{
+New-Item $directory -type directory
+}
+ 
+# Move File to new location
+$file | Move-Item -Destination $Directory
+=======
+﻿# Get the files which should be moved, without folders
+$files = Get-ChildItem "H:\## Camera Uploads ##\" -Recurse | where {!$_.PsIsContainer}
+ 
+# List Files which will be moved
+$files
+ 
+# Target Folder where files should be moved to. The script will automatically create a folder for the year and month.
+$targetPath = "H:\## Camera Uploads ##\"
+ 
+foreach ($file in $files)
+{
+# Get year and Month of the file
+# I used LastWriteTime since this are synced files and the creation day will be the date when it was synced
+$year = $file.LastWriteTime.Year.ToString()
+$month = $file.LastWriteTime.Month.ToString()
+ 
+# Out FileName, year and month
+$file.Name
+$year
+$month
+ 
+# Set Directory Path
+$Directory = $targetPath + "\" + $year + "\" + $month
+# Create directory if it doesn't exsist
+if (!(Test-Path $Directory))
+{
+New-Item $directory -type directory
+}
+ 
+# Move File to new location
+$file | Move-Item -Destination $Directory
+>>>>>>> 3beb25b9c2944c22bb5d139140b660921325d8c8
+}
